0ff1a74b8fd65ffc92d7d184cb3b46b90ce13ab4
Dominik Bruhn <Dominik.Bruhn@signal-iduna.de>
2021-10-22 18:29:00 +0200

    kopk

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,26 @@ const client: MongoClient = new MongoClient(databaseConnectionString);
 const app = express();
 app.use(express.json());
 
+// Add headers before the routes are defined
+app.use(express.json(), function (_req, res, next) {
+  
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', "true");
+
+  // Pass to next layer of middleware
+  next();
+});
+
 const openApiDocPath: any = {};
 
 function createEndpoints() {
